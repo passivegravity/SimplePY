b939631204cfc1c87b2b97425f7473025fd641ef
Simon <96681438+LowOnGravity@users.noreply.github.com>
2023-03-31 18:14:59 +0200

    Add files via upload

---
+++ Math/Arithmetic.py
@@ -0,0 +1,10 @@
+# Arithmetic Wage
+
+# Simplest Way possible, I am really unwilling when it comes to PY.
+a = float(input("Provide first number: "))
+b = float(input("Provide second number: "))
+c = float(input("Provide third number: "))
+d = float(input("Provide fourth number: "))
+e = float(input("Provide fifth number: "))
+print ("The result is: ", (a+b+c+d+e)/5) # Divided by it's amount. 
+    
