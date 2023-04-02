e505d81552202f5d91d77ce595d380829dac031a
Simon <96681438+LowOnGravity@users.noreply.github.com>
2023-04-02 14:02:19 +0200

    Add files via upload

---
+++ Math/EvenOdd.py
@@ -0,0 +1,14 @@
+# Odd or Even
+
+num = 1
+
+while(num < 400):
+    print(f'elif num == {num}:')
+    if (num % 2 == 0):
+        print('\tprint(\'Even\')')
+    else:
+         print('\tprint(\'Odd\')')
+    num += 1;
+
+
+    
